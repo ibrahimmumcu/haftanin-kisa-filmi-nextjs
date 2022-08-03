fec9852a2a9ed370493011432b757cc4ca38a5ea
Ibrahim Mumcu <mumcuibrahim@gmail.com>
2022-08-03 21:12:01 +0200

    use same api endpoint for detail page so we can benefit server-side-cache by requesting data from the same url

--- pages/film/[link].tsx
+++ pages/film/[link].tsx
@@ -4,13 +4,22 @@ import fetcher from "../../lib/fetcher";
 import Header from "../../components/Header";
 import FilmPlayer from "../../components/FilmPlayer";
 import Skeleton from "../../components/Skeleton";
+import { FilmsResponse } from "../../interfaces/films-response.interface";
 
 type FilmDetailProps = {
   link: string;
 };
 
 export default function FilmDetail({ link }: FilmDetailProps) {
-  const { data, error } = useSWR<Film>("/api/film/" + link, fetcher);
+  const { data, error } = useSWR<FilmsResponse>(
+    "/api/all?page=1&sortBy=latest&perPage=10000",
+    fetcher
+  );
+
+  let filmDetail;
+  if (data) {
+    filmDetail = data.data.find((film) => film.link === link) as Film;
+  }
 
   if (error) return <div>failed to load</div>;
   //if (!data) return <div>loading...</div>;
@@ -23,7 +32,9 @@ export default function FilmDetail({ link }: FilmDetailProps) {
         if (!data) {
           return <Skeleton type="filmDetail" />;
         } else {
-          return <FilmPlayer key={data.link} film={data} />;
+          return (
+            <FilmPlayer key={filmDetail?.link} film={filmDetail as Film} />
+          );
         }
       })()}
     </>
